2eadd716e20ebce73ae642be5d61aee4af99ff5c
TaMeR <gruessle@gmail.com>
2018-07-05 16:32:41 +0300

    working

--- main.go
+++ main.go
@@ -8,14 +8,44 @@ import (
 	"strings"
 )
 
+var version struct {
+	Long  string
+	Tags  string
+	Git   string
+	Dirty bool
+}
+
 func main() {
-	cmd := exec.Command("git", "describe", "--always", "--long", "--dirty")
-	cmd.Stdin = strings.NewReader("Vesion")
+	cmd := exec.Command("git", "describe", "--always", "--long", "--tags", "--dirty")
+	cmd.Stdin = strings.NewReader("Version")
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err := cmd.Run()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error: %s", err.Error())
 	}
-	fmt.Printf("in all caps: %q\n", out.String())
+	version.Long = out.String()
+	vers := strings.Split(version.Long, "-")
+	l := len(vers)
+	if strings.Contains(vers[l-1], "dirty") {
+		fmt.Printf("Is Dirty")
+		version.Dirty = true
+		vers = vers[0 : l-1]
+		fmt.Printf("Contants dirty new vers %s", vers)
+	}
+	l = len(vers)
+	version.Git = vers[l-1]
+	vers = vers[0 : l-1]
+	l = len(vers)
+
+	if len(vers) == 2 {
+		fmt.Println("vers length 2")
+		version.Tags = vers[0]
+	}
+
+	fmt.Printf("\nVers: %s\n", vers)
+	fmt.Printf("version Long: %v\n", version.Long)
+	fmt.Printf("\nGit: %s\n", version.Git)
+	fmt.Printf("\nTags: %s\n", version.Tags)
+	fmt.Printf("\nDirty: %v\n", version.Dirty)
 }
