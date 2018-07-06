851a57757e9772d454db12f4545ebd3e19386cb8
TaMeR <gruessle@gmail.com>
2018-07-06 16:59:16 +0300

    code block

--- doc.go
+++ doc.go
@@ -8,11 +8,11 @@
 //
 // cat versioninfo.go
 /*
-package main
-const VerLong = 0.2-6-ge29c9a3-dirty
+   package main
+   const VerLong = 0.2-6-ge29c9a3-dirty
 
-const VerTag = 0.2
-const VerGit = ge29c9a3
-const VerModTime = 2018-07-06 13:37:03.28865131 +0300 +03
+   const VerTag = 0.2
+   const VerGit = ge29c9a3
+   const VerModTime = 2018-07-06 13:37:03.28865131 +0300 +03
 */
 package version
