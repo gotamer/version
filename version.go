80e8dd8b5d30f26529dcef6480c47d0703f8e1e2
TaMeR <gruessle@gmail.com>
2018-07-06 19:55:58 +0300

    remove unnessesery stuff

--- version.go
+++ version.go
@@ -7,97 +7,63 @@ import (
 	"os"
 	"os/exec"
 	"strings"
-	"time"
 )
 
-const gitfile = ".git/COMMIT_EDITMSG"
-
-// Version holds version info from ´git describe´
-var Version struct {
-	Long    string
-	Tags    string
-	Git     string
-	Dirty   bool
-	ModTime time.Time
-}
-
 func init() {
-	if _, err := os.Stat(gitfile); !os.IsNotExist(err) {
-		version()
-		modTime()
-		write()
-	}
-}
-
-// Long version from Git
-func Long() string {
-	return Version.Long
-}
-
-// Tag version from Git
-func Tag() string {
-	return Version.Tags
-}
-
-// Git returns the 7 hexadecimal digits version from Git
-func Git() string {
-	return Version.Tags
-}
-
-// ModTime returns the last Git commit time
-func ModTime() time.Time {
-	return Version.ModTime
+	version()
 }
 
 // Version gets version info from git describe
 func version() {
-	var err error
+	fi, err := os.Stat(".git/COMMIT_EDITMSG")
+	if os.IsNotExist(err) {
+		return
+	}
+	var buf bytes.Buffer
+	buf.WriteString("package main\n\n")
+	buf.WriteString("VarModTime is a UTC Unix time stamp\n")
+	buf.WriteString(fmt.Sprintf("const VerModTime = %d\n\n", fi.ModTime().UTC().Unix()))
+
 	cmd := exec.Command("git", "describe", "--always", "--long", "--tags", "--dirty")
 	cmd.Stdin = strings.NewReader("Version")
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err = cmd.Run()
+
+	var long = out.String()
+	long = strings.Trim(long, "\n ")
+
 	if err != nil {
 		log.Printf("Version : Error: %s", err.Error())
 	} else {
-		Version.Long = out.String()
-		vers := strings.Split(Version.Long, "-")
+		buf.WriteString("VarLong is the full version from Git command output\n")
+		buf.WriteString(fmt.Sprintf("const VerLong = \"%s\"\n\n", long))
+
+		vers := strings.Split(long, "-")
 		l := len(vers)
+		buf.WriteString("VarDirty means app was build with a git dir that contained modifications which had not been committed.\n")
 		if strings.Contains(vers[l-1], "dirty") {
-			Version.Dirty = true
+			buf.WriteString(fmt.Sprintf("const VerDirty = %s\n\n", "true"))
 			vers = vers[0 : l-1]
+		} else {
+			buf.WriteString(fmt.Sprintf("const VerDirty = %s\n\n", "false"))
 		}
 		l = len(vers)
-		Version.Git = vers[l-1]
+		buf.WriteString("VarGit is the 7 hexadecimal digits version from Git.\n")
+		buf.WriteString(fmt.Sprintf("const VerGit = \"%s\"\n\n", vers[l-1]))
 		vers = vers[0 : l-1]
 
 		if len(vers) == 2 {
-			Version.Tags = vers[0]
+			buf.WriteString("VarTag is the Tag version from Git.\n")
+			buf.WriteString(fmt.Sprintf("const VerTag = \"%s\"\n", vers[0]))
 		}
 	}
-}
 
-func modTime() {
-	fi, err := os.Stat(gitfile)
-	if err != nil {
-		log.Printf("Version : Error: %s", err.Error())
-	}
-	Version.ModTime = fi.ModTime()
-}
-
-func write() {
-	var buf bytes.Buffer
-	buf.WriteString("package main\n")
-	buf.WriteString(fmt.Sprintf("const VerLong = %s\n", Version.Long))
-	buf.WriteString(fmt.Sprintf("const VerTag = %s\n", Version.Tags))
-	buf.WriteString(fmt.Sprintf("const VerGit = %s\n", Version.Git))
-	buf.WriteString(fmt.Sprintf("const VerModTime = %s\n", Version.ModTime))
-
-	//os.Remove("versioninfo.go")
 	f, err := os.Create("versioninfo.go")
 	if err != nil {
 		log.Printf("Version : file : Error: %s", err.Error())
 	}
 	defer f.Close()
 	buf.WriteTo(f)
+
 }
--- version_test.go
+++ version_test.go
@@ -6,10 +6,6 @@ import (
 )
 
 func TestRunMain(t *testing.T) {
-	log.Printf("Version Long: %v\n", Version.Long)
-	log.Printf("Git: %s\n", Version.Git)
-	log.Printf("Tags: %s\n", Version.Tags)
-	log.Printf("Dirty: %v\n", Version.Dirty)
-	log.Printf("Time: %s\n", Version.ModTime)
+	log.Printf("Version Long: %v\n", "test")
 
 }
