eb8c22d5d8a20a07b2de261efad5a296d5930120
TaMeR <gruessle@gmail.com>
2022-05-26 14:24:45 +0300

    created verup readme

--- version.go
+++ version.go
@@ -1,16 +1,16 @@
 package main
 
 //VarModTime is a UTC Unix time stamp
-const VerModTime = 1653563488
+const VerModTime = 1653564243
 
 //VarLong is the full version from Git command output
-const VerLong = "1.1.0-0-g9b0d257"
+const VerLong = "1.1.0-2-g0cdfa81"
 
 //VarDirty means app was build with a git dir that contained modifications which had not been committed.
 const VerDirty = false
 
 //VarGit is the 7 hexadecimal digits version from Git.
-const VerGit = "g9b0d257"
+const VerGit = "g0cdfa81"
 
 //VarTag is the Tag version from Git.
 const VerTag = "1.1.0"
